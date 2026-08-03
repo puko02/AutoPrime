81f18a82b66952c6c2d9debe3741be7a1c0d6354
Alex <burraum55@gmail.com>
2026-08-03 20:31:19 -0300

    instalacoes das dependencias e springboot

---
+++ Auto Prime/app/src/test/java/com/AutoPrime/Prime/PrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoPrime.Prime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
