d619b0e11162b657a03d4b340c5552037e21a382
Guillermo <guillermotoro61@gmail.com>
2026-08-04 21:17:08 -0300

    Start

--- Auto Prime/app/src/main/java/com/autoprime/App/PrimeApplication.java
+++ Auto Prime/app/src/main/java/com/autoprime/App/PrimeApplication.java
@@ -1,5 +1,4 @@
-package com.AutoPrime.Prime;
-
+package com.autoprime.App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
